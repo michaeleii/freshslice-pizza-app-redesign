7c2e00fcebf96d8185fd86d8becd15984cdf81bf
Michael <michaelleie0ca@gmail.com>
2024-02-05 19:05:26 -0800

    Finish navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,27 +4,39 @@ import Link from "next/link";
 export default function NavBar() {
   return (
     <nav className="fixed bottom-0 w-full bg-lime-950">
-      <ul className="mx-auto flex h-20 max-w-xl items-center justify-around border-t bg-white text-gray-500">
+      <ul className="mx-auto flex h-20 max-w-xl items-center justify-around border-t bg-gray-50 text-gray-500 shadow-lg">
         <li>
-          <Link href="/" className="flex flex-col items-center">
+          <Link
+            href="/"
+            className="flex flex-col items-center transition-colors hover:text-green-500"
+          >
             <HomeIcon />
             <span>Home</span>
           </Link>
         </li>
         <li>
-          <Link href="/order" className="flex flex-col items-center">
+          <Link
+            href="/order"
+            className="flex flex-col items-center transition-colors hover:text-green-500"
+          >
             <PizzaIcon />
             <span>Order</span>
           </Link>
         </li>
         <li>
-          <Link href="/rewards" className="flex flex-col items-center">
+          <Link
+            href="/rewards"
+            className="flex flex-col items-center transition-colors hover:text-green-500"
+          >
             <CreditCardIcon />
             <span>Rewards</span>
           </Link>
         </li>
         <li>
-          <Link href="/account" className="flex flex-col items-center">
+          <Link
+            href="/account"
+            className="flex flex-col items-center transition-colors hover:text-green-500"
+          >
             <User2Icon />
             <span>Account</span>
           </Link>
