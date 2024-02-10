80e80bda1f8e901728fa0b7ce9a1249de7f11d5f
Michael <michaelleie0ca@gmail.com>
2024-02-09 22:43:46 -0800

    Add delivery options

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,17 +1,32 @@
 import MainWrapper from "@/components/main-wrapper";
 import { Button } from "@/components/ui/button";
 
+const Buttons = [
+  {
+    text: "Delivery",
+  },
+  {
+    text: "Pickup",
+  },
+  {
+    text: "Gift Card",
+  },
+];
+
 export default function Order() {
   return (
     <MainWrapper className="flex flex-col items-center justify-center">
       <h1 className="mb-4 text-2xl font-semibold">Choose an option</h1>
-      <div className="flex w-full max-w-xs flex-col gap-2">
-        <Button className="rounded-lg bg-green-500 p-4 text-white" size="lg">
-          Delivery
-        </Button>
-        <Button className="rounded-lg bg-green-500 p-4 text-white">
-          Pickup
-        </Button>
+      <div className="flex w-full max-w-xs flex-col gap-4">
+        {Buttons.map((button) => (
+          <Button
+            key={button.text}
+            className="h-full rounded-lg bg-green-500 p-4 text-2xl text-white"
+            size="lg"
+          >
+            {button.text}
+          </Button>
+        ))}
       </div>
     </MainWrapper>
   );
