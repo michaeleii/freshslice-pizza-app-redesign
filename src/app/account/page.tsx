80a4979141e81bbaeb616fb6067991ea5500aea5
Michael <michaelleie0ca@gmail.com>
2024-02-13 11:49:53 -0800

    Add drawer to app

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,5 +1,28 @@
 import MainWrapper from "@/components/main-wrapper";
+import { ArrowRightIcon, User2Icon } from "lucide-react";
+
+const SettingLinks = [];
 
 export default function Account() {
-  return <MainWrapper>Account</MainWrapper>;
+  return (
+    <MainWrapper>
+      <div></div>
+      <div className="flex flex-col shadow">
+        <div className="flex justify-between border-b p-5">
+          <div className="flex items-center gap-2">
+            <User2Icon />
+            <span>Edit Account Information</span>
+          </div>
+          <ArrowRightIcon />
+        </div>
+        <div className="flex justify-between border-b p-5">
+          <div className="flex items-center gap-2">
+            <User2Icon />
+            <span>Edit Account Information</span>
+          </div>
+          <ArrowRightIcon />
+        </div>
+      </div>
+    </MainWrapper>
+  );
 }
