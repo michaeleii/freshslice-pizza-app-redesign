99683aebb30cd8a34044d1f9f5d1fb481039dcf3
Michael Lei <michaelleie0ca@gmail.com>
2024-02-13 18:34:39 -0800

    Add pizza images

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,9 @@
+import MainWrapper from "@/components/main-wrapper";
+
+export default function MenuPage() {
+  return (
+    <MainWrapper>
+      <h1>Menu</h1>
+    </MainWrapper>
+  );
+}
--- src/app/rewards/page.tsx
+++ src/app/rewards/page.tsx
@@ -8,74 +8,67 @@ import Coupons from "@/components/coupons";
 
 export default function Rewards() {
   return (
-    <>
-      <h1 className="mx-auto mb-4 w-full max-w-md bg-black py-2 text-center text-white">
-        Rewards
-      </h1>
-      <MainWrapper>
-        <div className="rounded-b-lg shadow-md">
-          <div className="rounded-t-lg border-t bg-green-600 py-2">
-            <Image
-              src={FreshSliceLogo}
-              alt="FreshSlice Logo"
-              height={150}
-              width={150}
-              className="mx-auto"
-            />
-            <div className="mt-2 text-center text-2xl uppercase tracking-widest text-white">
-              Rewards
-            </div>
-          </div>
-          <div className="p-5">
-            <div className="flex flex-col items-center gap-2">
-              <span className="text-xl font-semibold text-green-600">
-                $1.05
-              </span>
-              <span className="text-xs uppercase text-gray-500">Balance</span>
-            </div>
-          </div>
-        </div>
-        <div className="my-5 rounded-lg border p-5">
+    <MainWrapper>
+      <div className="rounded-b-lg shadow-md">
+        <div className="rounded-t-lg border-t bg-green-600 py-2">
           <Image
-            src={QRCode}
-            alt="QR Code"
-            height={200}
-            width={200}
+            src={FreshSliceLogo}
+            alt="FreshSlice Logo"
+            height={150}
+            width={150}
             className="mx-auto"
           />
-          <div className="mt-4 text-center text-sm uppercase tracking-widest text-gray-500">
-            Scan to earn rewards
+          <div className="mt-2 text-center text-2xl uppercase tracking-widest text-white">
+            Rewards
           </div>
         </div>
-        <div className="my-5 p-5">
-          <div className="mx-auto mt-4 w-fit rounded-full bg-green-600 p-4 text-center text-7xl uppercase text-white">
-            5%
-          </div>
-          <div className="mt-4 text-center text-xl uppercase ">Cash Back</div>
-          <div className="mt-4 text-center text-sm text-gray-500">
-            on every purchase
+        <div className="p-5">
+          <div className="flex flex-col items-center gap-2">
+            <span className="text-xl font-semibold text-green-600">$1.05</span>
+            <span className="text-xs uppercase text-gray-500">Balance</span>
           </div>
         </div>
-        <div className=" flex flex-col gap-2">
-          <Button
-            className="flex w-full items-center gap-2 bg-green-600"
-            size="lg"
-          >
-            <GiftIcon />
-            <span>Purchase Gift Card</span>
-          </Button>
-          <Button
-            className="flex w-full items-center gap-2 bg-green-600"
-            size="lg"
-          >
-            <PiggyBankIcon />
-            <span>Deposit Funds</span>
-          </Button>
+      </div>
+      <div className="my-5 rounded-lg border p-5">
+        <Image
+          src={QRCode}
+          alt="QR Code"
+          height={200}
+          width={200}
+          className="mx-auto"
+        />
+        <div className="mt-4 text-center text-sm uppercase tracking-widest text-gray-500">
+          Scan to earn rewards
         </div>
-        <div className="my-10">
-          <Coupons />
+      </div>
+      <div className="my-5 p-5">
+        <div className="mx-auto mt-4 w-fit rounded-full bg-green-600 p-4 text-center text-7xl uppercase text-white">
+          5%
         </div>
-      </MainWrapper>
-    </>
+        <div className="mt-4 text-center text-xl uppercase ">Cash Back</div>
+        <div className="mt-4 text-center text-sm text-gray-500">
+          on every purchase
+        </div>
+      </div>
+      <div className=" flex flex-col gap-2">
+        <Button
+          className="flex w-full items-center gap-2 bg-green-600"
+          size="lg"
+        >
+          <GiftIcon />
+          <span>Purchase Gift Card</span>
+        </Button>
+        <Button
+          className="flex w-full items-center gap-2 bg-green-600"
+          size="lg"
+        >
+          <PiggyBankIcon />
+          <span>Deposit Funds</span>
+        </Button>
+      </div>
+      <div className="my-10">
+        <Coupons />
+      </div>
+    </MainWrapper>
   );
 }
